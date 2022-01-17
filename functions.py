971d1a2300457ebced37fc3d233218e966cb0cb4
Your Name  git config --global user.email you@example.comgit config --global user.email holt.codes@gmail.com <you@example.com>
2022-01-16 19:25:57 -0800

    commit

--- functions.py
+++ functions.py
@@ -9,6 +9,15 @@ sports_directory = {
     'soccer' : ['Messi', 'Ronaldo', 'Rooney']
 }
 z = [ {'x': 10, 'y': 20} ]
+x[1][0] = 15
+print(x)
+students[0]['last_name'] = "Bryant"
+print(students)
+sports_directory['soccer'][0] = 'Andres'
+print(sports_directory)
+z[0]['y'] = 30
+print(z)
+
 
 # 2.Iterate Through a List of Dictionaries
 students = [
@@ -17,25 +26,25 @@ students = [
          {'first_name' : 'Mark', 'last_name' : 'Guillen'},
          {'first_name' : 'KB', 'last_name' : 'Tonel'}
     ]
-iterateDictionary(students) 
-# should output: (it's okay if each key-value pair ends up on 2 separate lines;
-# bonus to get them to appear exactly as below!)
-first_name - Michael, last_name - Jordan
-first_name - John, last_name - Rosales
-first_name - Mark, last_name - Guillen
-first_name - KB, last_name - Tonel
+def iterate_Dictionary(list):
+  for i in range(0, len(list)):
+    output = ""
+    for key,val in list[i].items():
+      output += f"  {key} - {val}"
+    print(output)
+
+iterate_Dictionary(students) 
 
 # 3.Get Values From a List of Dictionaries
 
-Michael
-John
-Mark
-KB
+def iterate_Dictionary2(key_name,list):
+  for i in range(0, len(list)):
+    for key,val in list[i].items():
+      if key == key_name:
+        print(val)
 
-Jordan
-Rosales
-Guillen
-Tonel
+iterate_Dictionary2('first_name', students) 
+iterate_Dictionary2('last_name', students) 
 
 
 # 4.Iterate Through a Dictionary with List Values
@@ -43,24 +52,13 @@ dojo = {
    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
 }
-printInfo(dojo)
-# output:
-7 LOCATIONS
-San Jose
-Seattle
-Dallas
-Chicago
-Tulsa
-DC
-Burbank
-    
-8 INSTRUCTORS
-Michael
-Amy
-Eduardo
-Josh
-Graham
-Patrick
-Minh
-Devon
+
+def print_info(dict):
+  for key,val in dict.items():
+    print(f"{len(val)} {key.upper()}")
+    for i in range(0, len(val)):
+      print(val[i])
+
+print_info(dojo)
+
 
